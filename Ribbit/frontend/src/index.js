08e26def2e55b979cf7275c6c36633396d378d11
Trivo <143166080+Trivo121@users.noreply.github.com>
2025-07-11 23:48:39 +0530

    Update index.js

--- Ribbit/frontend/src/index.js
+++ Ribbit/frontend/src/index.js
@@ -1,2 +1,10 @@
-const { ReactDOM } = window;
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '.src/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
